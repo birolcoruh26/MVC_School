809211883e62a19066b1f53429f29cfb474e0b19
TitanForce99 <birolcoruh26@gmail.com>
2022-02-06 17:55:08 +0300

    Add project files.

---
+++ MVC_001/Controllers/StudentController.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+using MVC_001.DataAccess;
+using MVC_001.ModelBase;
+using MVC_001.Models;   // Models altındaki sınıfları kullanacağız.
+using MVC_001.Security;
+
+namespace MVC_001.Controllers
+{
+    public class StudentController : Controller
+    {        
+        //public ActionResult Index_Old()
+        //{   // model yolu ile View'e veri gönderme
+
+        //    Student stdnt = new Student()
+        //    {
+        //        ID = 99,
+        //        Name = "Burak",
+        //        Surname = "Yılmaz"
+        //    };
+
+        //    return View(stdnt);// /Views/Student/Index.cshtml'i çalıştır.
+        //}
+        public ActionResult Index()
+        {
+            List<Student> students = StudentDAL.Methods.List();
+
+            return View(students);
+        }
+
+        public ActionResult About()
+        {
+            return RedirectToAction("Index"); // Yukarıdaki Index controller'ına yönlendir.
+        }
+
+
+        // Yeni öğrenci kayıt formunu göster.
+        
+        [CustomAuthorize(Roles="teacher")]
+        public ActionResult Add()
+        {
+            Student newStudent = new Student();
+            return View(newStudent);
+        }
+
+        [HttpPost]
+        public ActionResult Add(Student student)
+        {
+            
+            TempData["insertedID"] = StudentDAL.Methods.Add(student);
+            return RedirectToAction("Index");
+        }
+        public ActionResult Edit(int id)
+        {
+            return View(StudentDAL.Methods.GetByID(id));
+        }
+
+        [HttpPost]
+        public ActionResult Edit(Student student)
+        {
+            int affectedRows = StudentDAL.Methods.Update(student);
+            if (affectedRows > 0)
+                TempData["editmessage"] = "Edit successfull!!!";
+            else    
+                TempData["editmessage"] = "Error on edit!!!";
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Details(int id)
+        {
+            return View(StudentDAL.Methods.GetByID(id));
+        }
+
+        [Authorize]
+        public ActionResult Delete(int id)
+        {
+            return View(StudentDAL.Methods.GetByID(id));
+        }
+
+        [HttpPost]
+        public ActionResult Delete(Student student)
+        {
+            int affectedRows = StudentDAL.Methods.Delete(student.ID);
+            if (affectedRows > 0)
+                TempData["deletemessage"] = "Delete successfull!!!";
+            else
+                TempData["deletemessage"] = "Error on delete!!!";
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        public ActionResult Index(string searchterm)
+        {
+            List<Student> searchedStudents = StudentDAL.Methods.Search(searchterm);
+            return View(searchedStudents);
+        }
+
+
+        public ActionResult Login(string returnUrl="/Student/Index")
+        {
+            ViewBag.ReturnUrl = returnUrl;
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(string email, string password,string returnUrl)
+        {
+            Student std = StudentDAL.Methods.Login(email, password);
+            //FormsAuthentication.SetAuthCookie("userlogin", false);
+            Role rl = std.Role;
+            
+            if(std.ID==0) // nesne boş ise
+            {
+                ViewBag.Error = "Login failed.";
+                return View("Login");
+            }
+            SessionPersister.Email = std.Email;
+            return Redirect(returnUrl);
+        }
+    }
+}
---
+++ MVC_001/DataAccess/StudentDAL.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using MVC_001.Models;
+
+namespace MVC_001.DataAccess
+{
+    public class StudentDAL
+    {
+        private static StudentDAL _Methods { get; set; }
+        public static StudentDAL Methods
+        {
+            get
+            {
+                if (_Methods == null)
+                    _Methods = new StudentDAL();
+                return _Methods;
+            }
+        }
+
+        public object Add(Student student)
+        {
+            string query = $"INSERT INTO Student (Name,Surname) VALUES(@name,@surname); SELECT SCOPE_IDENTITY();";
+            SqlCommand cmd = new SqlCommand(query, DbTools.dbConnection);
+            cmd.Parameters.AddWithValue("@name", student.Name);          
+            cmd.Parameters.AddWithValue("@surname", student.Surname);
+
+            return DbTools.Methods.Add(cmd);
+
+        }
+
+        public List<Student> List()
+        {
+            string query = $"SELECT * FROM Student;";
+            SqlCommand cmd = new SqlCommand(query, DbTools.dbConnection);
+            return GetStudentList(cmd);
+        }
+
+        private static List<Student> GetStudentList(SqlCommand cmd)
+        {
+            List<Student> studentList = new List<Student>();
+            IDataReader reader;
+            try
+            {
+                DbTools.Methods.ConnectDB();
+                reader = cmd.ExecuteReader();
+                while (reader.Read())
+                {
+                    studentList.Add(
+                    new Student()
+                    {
+                        ID = reader.GetInt32(0),
+                        Name = reader["Name"].ToString(),
+                        Surname = reader["Surname"].ToString(),
+                        Email = reader["Email"].ToString(),
+                        Password = reader["Password"].ToString(),
+                        RoleID = int.Parse(reader["RoleID"].ToString())
+                    });
+                }
+                DbTools.Methods.DisconnectDB();
+            }
+            catch
+            {
+                throw;
+            }
+            return studentList;
+        }
+
+        public Student GetByID(int id)
+        {
+            string query = $"SELECT * FROM Student WHERE ID=@id;";
+            SqlCommand cmd = new SqlCommand(query, DbTools.dbConnection);
+            cmd.Parameters.AddWithValue("@id", id);
+            try
+            {
+                return GetStudentList(cmd)[0];
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+        public Student Login(string email,string password)
+        {
+            string query = $"SELECT * FROM Student WHERE Email='{email}' AND Password='{password}'"; ;
+            SqlCommand cmd = new SqlCommand(query, DbTools.dbConnection);
+            // Addwithvalue eklenecek.
+            try
+            {
+                return GetStudentList(cmd)[0];
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+
+
+        public int Update(Student student)
+        {
+            string query = $"UPDATE Student SET Name=@name,Surname=@surname WHERE ID=@id;";
+            SqlCommand cmd = new SqlCommand(query, DbTools.dbConnection);
+            cmd.Parameters.AddWithValue("@name", student.Name);
+            cmd.Parameters.AddWithValue("@surname", student.Surname);
+            cmd.Parameters.AddWithValue("@id", student.ID);
+            return DbTools.Methods.Execute(cmd);
+
+        }
+        public int Delete(int id)
+        {
+            string query = $"DELETE FROM Student WHERE ID=@id;";
+            SqlCommand cmd = new SqlCommand(query, DbTools.dbConnection);
+            cmd.Parameters.AddWithValue("@id", id);
+            return DbTools.Methods.Execute(cmd);
+        }
+
+        public List<Student> Search(string searchterm)
+        {
+            string query = $"SELECT * FROM Student WHERE Name LIKE '%' + @searchterm + '%' OR Surname LIKE '%' + @searchterm + '%';";
+            SqlCommand cmd = new SqlCommand(query, DbTools.dbConnection);
+            cmd.Parameters.AddWithValue("@searchterm", searchterm);
+            try
+            {
+                return GetStudentList(cmd);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+    }
+}
